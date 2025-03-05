6d154f207058978cf17e7ff109045fd8c11d389c
Oriemi Obang <oriemiobango@gmail.com>
2025-03-05 15:35:38 +0300

    changes

--- frontend/src/hook/useSignup.js
+++ frontend/src/hook/useSignup.js
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import { useAuthContext } from "./useAuthContext" 
-import dotenv from "dotenv";
-dotenv.config();
 
 
 export const useSignup = ()=> {
--- frontend/src/pages/VerseCompare.js
+++ frontend/src/pages/VerseCompare.js
@@ -3,8 +3,7 @@ import { useLocation } from "react-router-dom";
 import { PulseLoader } from 'react-spinners';
 import { GoArrowLeft } from "react-icons/go";
 import { Link } from "react-router-dom"
-import dotenv from "dotenv";
-dotenv.config();
+
 
 const VerseCompare = ()=> {
 
